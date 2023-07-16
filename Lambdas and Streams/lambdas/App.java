56bb42b54bb268799f4db000bdbf7d75950cf842
ppdmartell <ppdmartell@gmail.com>
2023-07-15 21:07:39 -0300

    Add Lambdas and Streams.

---
+++ Lambdas and Streams/lambdas/App.java
@@ -0,0 +1,16 @@
+/*
+This file contains some knowledge about lambdas which are part of functional programming.
+
+Resources:
+[1] https://www.youtube.com/watch?v=gpIUfj3KaOc&list=PLqq-6Pq4lTTa9YGfyhyW2CqdtW9RtY-I3
+[2] https://www.youtube.com/watch?v=HsOVdmmBS9E
+
+* Resource [1] is a playlist.
+
+*/
+
+class App {
+	public static void main(String[] args) {
+		
+	}
+}
