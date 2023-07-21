dd44e53c213b9fa5aa6cc2a9a6c64bd92eca63e4
ppdmartell <ppdmartell@gmail.com>
2023-07-21 10:28:21 -0300

    Update App.java

--- Concurrency and Multithreading/The Executor Framework/App.java
+++ Concurrency and Multithreading/The Executor Framework/App.java
@@ -14,8 +14,12 @@ an interesting explanation can be seen in [1].
 Resources:
 [1] https://chat.openai.com/c/d77e13c2-40b0-42ff-8310-eb84fd399ba4 [search phrase: "one-size-fits-all answer"]
 [2] https://www.youtube.com/watch?v=sUVJoUp8gBc
+[3] https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/Executors.html#newWorkStealingPool--
 */
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -50,6 +54,21 @@ class App {
 			e.printStackTrace();
 		}
 		executorService3.shutdown();
+
+		//Use ExecutorService and invokeAll() to submit a list of tasks with a pool with as many threads as CPU cores.
+		ExecutorService executorService4 = Executors.newWorkStealingPool(); //[3] Creates a work-stealing thread pool using all available processors as its target parallelism level.
+		List<Callable<String>> tasks = Arrays.asList(() -> "Task number 1", () -> "Task number 2", () -> "Task number 3");
+		try {
+			for(Future<String> future : executorService4.invokeAll(tasks)) {
+				System.out.println("Task: " + future.get());
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} finally {
+			executorService4.shutdown();
+		}
 	}
 }
 
