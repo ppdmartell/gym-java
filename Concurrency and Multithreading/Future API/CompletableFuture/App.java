5fb923aca545b8d8302ca456b199a62e523ce4d0
ppdmartell <ppdmartell@gmail.com>
2023-07-25 23:00:43 -0300

    Update App.java

--- Concurrency and Multithreading/Future API/CompletableFuture/App.java
+++ Concurrency and Multithreading/Future API/CompletableFuture/App.java
@@ -22,10 +22,12 @@ Resources:
 [1] https://www.youtube.com/watch?v=xpjvY45Hbyg
 [2] https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/CompletableFuture.html
 [3] https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/Future.html
+[4] https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
 */
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 class App {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
@@ -34,7 +36,48 @@ class App {
 		System.out.println(cFuture.getNow("SoccerNow"));  //This basically giving a default value if the result is not ready, and won't consider the CompletableFuture complete.
 		cFuture.complete("Soccer");
 		System.out.println(cFuture.get());  //This .get() method will (like Future interface) block the execution until getting the result.
+		System.out.println("------------------------------------------------------------------");
 
-		//Executing code asynchronously
+		//Executing a Runnable code asynchronously with runAsync() method.
+		System.out.printf("Main thread: %s%n", Thread.currentThread().getName());
+		CompletableFuture<Void> cFuture2 = CompletableFuture.runAsync(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(2);
+				System.out.printf("Future thread: %s%n", Thread.currentThread().getName());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		cFuture2.get();
+		System.out.println("------------------------------------------------------------------");
+
+		//Executing a Supplier (see [4] for functional interfaces) code Asynchronously with supplyAsync() method.
+		System.out.printf("Main thread: %s%n", Thread.currentThread().getName());
+		CompletableFuture<String> cFuture3 = CompletableFuture.supplyAsync(() -> {
+			try {
+    			TimeUnit.SECONDS.sleep(2);
+    			return "Future thread: " +  Thread.currentThread().getName();  //See a return (Supplier) instead of a void statement as the previous example.
+			} catch (InterruptedException e) {
+    			e.printStackTrace();
+				return "Error occurred";
+			}
+		});
+		System.out.println(cFuture3.get());
+		System.out.println("------------------------------------------------------------------");
+
+		/*
+		So far, CompletableFuture is basically behaving as a Future
+		and this is not the purpose. Let's harness the true capabilities of
+		CompletableFuture with its enhanced features.
+		*/
+
+		//Executing chained operations
+		CompletableFuture<String> cFuture4 = CompletableFuture.supplyAsync(() -> "Result X...")
+														.thenApply(resultX -> resultX + "processed and turned into Result Y...")
+														.thenApply(resultY -> resultY + "processed, execution completed!");
+
+		String result = cFuture4.join(); //This can also be done with get() as in previous cFutures.
+		System.out.println(result);
+		System.out.println("------------------------------------------------------------------");
 	}
 }
