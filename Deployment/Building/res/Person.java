d171bde36e7b9630dd08a0e5f595f279a98c8dca
ppdmartell <ppdmartell@gmail.com>
2023-08-01 10:36:39 -0300

    Example of creating a JAR file and link it to App.java (later App.class) for execution.

---
+++ Deployment/Building/res/Person.java
@@ -0,0 +1,13 @@
+class Person {
+	private final String name;
+	private final String lastname;
+
+	public Person(String name, String lastname) {
+		this.name = name;
+		this.lastname = lastname;
+	}
+
+	public String getName() { return name; }
+
+	public String getLastname() { return lastname; }
+}
