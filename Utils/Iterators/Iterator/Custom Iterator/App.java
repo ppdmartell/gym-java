d31b802fc8102a6955112d59dfed2564a8c61915
ppdmartell <ppdmartell@gmail.com>
2023-08-02 09:14:39 -0300

    Add a custom Iterator (CustomListIterator) example.

---
+++ Utils/Iterators/Iterator/Custom Iterator/App.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+class App {
+	public static void main(String[] args) {
+		List<Person> list = new ArrayList<>(Arrays.asList(
+										new Person("Lionel", "Messi"),
+										new Person("Frenkie", "De Jong"),
+										new Person("Carles", "Pujol"),
+										new Person("Ronald", "Araujo")));
+
+		CustomListIterator<Person> it = new CustomListIterator<>(list);  //Interesting, I was using "new CustomListIterator(list)", without the diamong operator and it's called raw type.
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+}
+
+class CustomListIterator<T> implements Iterator<T> {
+
+	private List<T> source = null;
+	private int index = 0;
+
+	public CustomListIterator(List<T> source) {
+		this.source = source;
+	}
+
+	@Override
+	public boolean hasNext() { return this.index < this.source.size(); }
+
+	@Override
+	public T next() { return this.source.get(index++); }
+}
+
+class Person {
+
+	private final String name;
+	private final String lastname;
+
+	public Person(String name, String lastname) {
+		this.name = name;
+		this.lastname = lastname;
+	}
+
+	public String getName() { return name; }
+
+	public String getLastname() { return lastname; }
+
+	@Override
+	public String toString() {
+		return "[name=" + name  + ",lastname=" + lastname  + "]";
+	}
+}
