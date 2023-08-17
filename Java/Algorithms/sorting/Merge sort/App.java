e3f39b27cca5fd26cd1dd3e16cebe5007f4fdc28
ppdmartell <ppdmartell@gmail.com>
2023-08-17 11:05:21 -0300

    Add Merge sort algorithm example.

---
+++ Java/Algorithms/sorting/Merge sort/App.java
@@ -0,0 +1,13 @@
+/*
+This example is about Merge sort and is based on [1].
+
+Resources:
+[1] https://www.youtube.com/watch?v=U4g1dMry4W4&list=PLlsmxlJgn1HLCmaF51i5xAbgv1f49CsoP&index=5
+[2] https://www.bigocheatsheet.com/
+*/
+
+class App {
+	public static void main(String[] args) {
+		
+	}
+}
