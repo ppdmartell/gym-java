319db98d8dbb14c62de389198837638e5f480801
ppdmartell <ppdmartell@gmail.com>
2023-08-22 16:37:18 -0300

    Add Counting sort algorithm example.

---
+++ Java/Algorithms/sorting/Counting sort/App.java
@@ -0,0 +1,12 @@
+/*
+This example is about Counting sort algorithm and is based on [1].
+
+Resources:
+[1] https://www.youtube.com/watch?v=YEabFTMDczQ&list=PLlsmxlJgn1HLCmaF51i5xAbgv1f49CsoP&index=7
+*/
+
+class App {
+    public static void main(String[] args) {
+        
+    }
+}
