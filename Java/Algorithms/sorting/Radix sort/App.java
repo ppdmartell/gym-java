9777797bed1315d1fa33994f8e05c5046faad870
ppdmartell <ppdmartell@gmail.com>
2023-09-02 20:42:02 -0300

    Add Radix sort algorithm example.

---
+++ Java/Algorithms/sorting/Radix sort/App.java
@@ -0,0 +1,9 @@
+/*
+This example is about Radix sort algorithm and is based on [1]. Radix sort comes to fix the problem
+Counting sort has, which is why creating a huge array to sort just a few elemets.
+Something important so notice is that Radix sort and Counting sort only work for the case you are
+trying to sort integers only.
+
+Resources:
+[1] https://www.youtube.com/watch?v=4ungd6NXFYI&list=PLlsmxlJgn1HLCmaF51i5xAbgv1f49CsoP&index=8
+*/
