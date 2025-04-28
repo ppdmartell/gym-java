861d126af5a3a6b24f49b9d3ef9603316a03c3a9
ppdmartell <ppdmartell@gmail.com>
2025-04-28 15:59:21 -0300

    Update .Example1.java.un~, Example1.java, and Example1.java~

--- Java/Courses/Udemy/Java Masterclass 2025 130+ Hours of Expert Lessons/Example1.java
+++ Java/Courses/Udemy/Java Masterclass 2025 130+ Hours of Expert Lessons/Example1.java
@@ -1,3 +1,9 @@
+/* Rules for declaring multiple variables in one statement
+ * - You cannot declare variables with different data types in a single statement.
+ * - If you declare multiple variables of the same data type in a single statement, you
+ *   must specify the data type only once before any variable names.
+ * */
+
 class Example1 {
     public static void main(String[] args) {
         byte myMinByteValue = Byte.MIN_VALUE, myMaxByteValue = Byte.MAX_VALUE;
