a1dece7a7dbfc2c95f90b84b952525611b020144
ppdmartell <ppdmartell@gmail.com>
2025-06-06 07:59:48 -0300

    In a same file, there should be only 1 public class. For example, PrimaryStudent can't be public as well.

---
+++ Java/Courses/Udemy/Java Masterclass 2025 130+ Hours of Expert Lessons/inheritance/Student/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+
+    public static void main(String[] args) {}
+
+}
+
+class PrimaryStudent {                                  // In a same file, there should be only 1 public class. For example, PrimaryStudent can't be public
+
+}
