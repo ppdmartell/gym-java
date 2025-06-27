6b0877a7d1b08f51ef5284cfbc2f9773a1e0b9e2
ppdmartell <ppdmartell@gmail.com>
2025-06-27 20:35:20 -0300

    Update Lambdas.java, Main.java, .Main.java.swp, and 3 more files

---
+++ Java/Courses/Udemy/Java Masterclass 2025 130+ Hours of Expert Lessons/lambdas_streams/external_material/passing the actual behavior (lambda expression)/Main.java
@@ -0,0 +1,17 @@
+class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
+
+
+
+// double number = 1.4;
+//
+// why if we could pass a behavior? Like:
+//
+// behvior = void doSomething() {
+//      System.out.println("Hello World!");
+// }
