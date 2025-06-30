038958af6488eaa32301ac1ed54e74db0fd9a47f
ppdmartell <ppdmartell@gmail.com>
2025-06-29 21:57:44 -0300

    run() doesn't start a new thread, just executes the method (lambda). On the other hand, start() creates a new thread and then executes run() inside that thread.

--- Java/Courses/Udemy/Java Masterclass 2025 130+ Hours of Expert Lessons/concurrency/external_material/Example1.java
+++ Java/Courses/Udemy/Java Masterclass 2025 130+ Hours of Expert Lessons/concurrency/external_material/Example1.java
@@ -2,7 +2,7 @@ class Example1 {
 
     public static void main(String[] args) {
         Thread t = new Thread(() -> System.out.println("Running inside the thread."));
-        t.run();
+        t.start();
         System.out.println("Execution continued besides the thread.");
     }
 
